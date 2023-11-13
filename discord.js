f137f8abd1492e03a9ea4950a4d848270bcbefea
shps18ppt <131682899+shps18ppt@users.noreply.github.com>
2023-11-13 18:35:09 +0800

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,48 +1,54 @@
-function authorize() {
-  window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=CLIENT_ID&redirect_uri=REDIRECT_URI&response_type=code&scope=email%20identify';
-}
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const { URLSearchParams } = require('url');
 
-function handleCallback() {
-  const code = new URLSearchParams(window.location.search).get('code');
+const clientId = '1173551794073387038';
+const clientSecret = 'secrets.CLIENT_SECRET';
+const redirectUri = 'https://castudios.h3l.fun/auth';
+const scopes = 'identify email';
+
+app.get('/auth', (req, res) => {
+  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scopes)}`;
+  res.redirect(discordAuthUrl);
+});
+
+app.get('/auth/callback', (req, res) => {
+  const code = req.query.code;
+  console.log('Authorization code:', code);
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    client_secret: clientSecret,
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri,
+    scope: scopes
+  });
 
   fetch('https://discord.com/api/oauth2/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: new URLSearchParams({
-      client_id: '1173551794073387038',
-      client_secret: 'secret.CLIENT_SECRET',
-      grant_type: 'authorization_code',
-      code: code,
-      redirect_uri: 'https://castudios.h3l.fun/auth',
-      scope: 'identify'
-    })
+    body: params
   })
   .then(response => response.json())
   .then(data => {
     const accessToken = data.access_token;
+    console.log('Access Token:', accessToken);
 
-    fetch('https://discord.com/api/v10/users/@me', {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    })
-    .then(response => response.json())
-    .then(userData => {
-      console.log(userData);
-    })
-    .catch(error => {
-      console.error('Error retrieving user data:', error);
-    });
+    // Use the access token for further API requests
+    // ...
+
+    res.send('Authentication successful!');
   })
   .catch(error => {
     console.error('Error exchanging authorization code:', error);
+    res.status(500).send('Error exchanging authorization code');
   });
-}
+});
 
-window.onload = () => {
-  if (window.location.search.includes('code=')) {
-    handleCallback();
-  }
-};
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
